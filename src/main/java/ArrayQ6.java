cba55a499c2278531219e726a2ae31f877b687ed
nette74 <spphire2@gmail.com>
2022-07-14 11:14:37 +0900

    Q6 complete

---
+++ src/main/java/ArrayQ6.java
@@ -0,0 +1,17 @@
+public class ArrayQ5 {
+    public static void main(String[] args) {
+        ArrayList ar = new ArrayList();
+
+        for ( int i = 0; i < 100; i++ ) {
+            ar.add((i + 1) * 10);
+        }
+
+        int ar_size = ar.size(); // ar_size의 값은 100 이어야 합니다.
+
+        for ( int i = 0; i < ar_size; i++ ) {
+            int value = ar.get(i);
+            System.out.println(value);
+        }
+
+    }
+}
