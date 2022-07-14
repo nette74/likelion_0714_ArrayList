be7697971576e40aba10514f9ec07aa5e08b4f1a
nette74 <spphire2@gmail.com>
2022-07-14 09:50:18 +0900

    Q2 improved

--- src/main/java/ArrayListMaker.java
+++ src/main/java/ArrayListMaker.java
@@ -5,36 +5,34 @@ public class ArrayListMaker {
         ar.add(200);
         ar.add(300);
 
-        int value = (int) ar.get(0);
+        int value = ar.get(0);
         System.out.println(value);
         // 출력 : 100
 
-        value = (int) ar.get(1);
+        value = ar.get(1);
         System.out.println(value);
         // 출력 : 200
 
-        value = (int) ar.get(2);
+        value = ar.get(2);
         System.out.println(value);
         // 출력 : 300
     }
 }
 class ArrayList {
-
-    int size;
-    Object[] arr;
+    private int size;
+    private Object[] arr;
     ArrayList(){
         size = 0;
         arr = new Object[size];
     }
     void add(Object d){
-        size+=1;
+        size=size+1;
         Object[] newarr = new Object[size];
-        if(size>1)
-            System.arraycopy(arr,0,newarr,0,size-1);
+        System.arraycopy(arr,0,newarr,0,size-1);
         newarr[size-1] = d;
         arr = newarr;
     }
-    Object get(int index){
-        return arr[index];
+    int get(int index){
+        return (int)arr[index];
     }
 }
